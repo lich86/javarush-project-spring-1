2ad6014dfc4b707ea9d5322ed590e52e14ddeb1a
Elizaveta Chervonnaya <ntropy@bk.ru>
2023-12-17 01:53:43 +0300

    code analysis

--- src/main/java/com/chervonnaya/dao/TaskDAO.java
+++ src/main/java/com/chervonnaya/dao/TaskDAO.java
@@ -1,14 +1,10 @@
 package com.chervonnaya.dao;
 
-import com.chervonnaya.model.Status;
 import com.chervonnaya.model.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 
 @Repository
